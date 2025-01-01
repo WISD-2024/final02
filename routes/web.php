53d809ed5f7fab109760885670dd855e821dd96e
陳炫樺 <chenxuanhua456@gmail.com>
2025-01-01 22:48:54 +0800

    安裝 Laravel Breeze套件做認證

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,20 +18,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::get('/', [ProductController::class, 'index']);  // 顯示商品列表給訪客
-//Route::get('login', [Auth\LoginController::class, 'showLoginForm']); //訪客進入登入畫面
-//Route::post('login', [Auth\LoginController::class, 'login']); //訪客登入
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+//賣家功能
 Route::middleware(['role:seller'])->group(function () {
-Route::get('seller/dashboard', [SellerController::class, 'dashboard']);
-Route::resource('seller/products', ProductController::class);}); //賣家管理商品
-
-//Route::middleware(['role:admin'])->group(function () {
-//Route::get('admin/dashboard', [AdminController::class, 'dashboard']);
-//Route::resource('admin/users', UserController::class);
-//Route::resource('admin/orders', OrderController::class);
-//});
-
+    Route::get('seller/dashboard', [SellerController::class, 'dashboard']);
+    Route::resource('seller/products', ProductController::class);
+});
+//賣家頁面路由
 use App\Http\Controllers\SellerController;
 
 Route::prefix('seller')->middleware(['auth', 'role:seller'])->group(function () {
